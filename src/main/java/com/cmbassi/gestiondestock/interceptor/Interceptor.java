ef97771ca75a8a7a044ec4a3d47709845f32fd48
lohan2015 <mbacyrille2002@yahoo.fr>
2022-04-05 13:25:24 +0100

    Ajout des exceptions

--- src/main/java/com/cmbassi/gestiondestock/interceptor/Interceptor.java
+++ src/main/java/com/cmbassi/gestiondestock/interceptor/Interceptor.java
@@ -10,18 +10,22 @@ public class Interceptor extends EmptyInterceptor {
   public String onPrepareStatement(String sql) {
     if (StringUtils.hasLength(sql) && sql.toLowerCase().startsWith("select")) {
       // select utilisateu0_.
-      final String entityName = sql.substring(7, sql.indexOf("."));
-      final String idEntreprise = MDC.get("idEntreprise");
-      if (StringUtils.hasLength(entityName)
-          && !entityName.toLowerCase().contains("entreprise")
-          && !entityName.toLowerCase().contains("roles")
-          && StringUtils.hasLength(idEntreprise)) {
+      try{
+        final String entityName = sql.substring(7, sql.indexOf("."));
+        final String idEntreprise = MDC.get("idEntreprise");
+        if (StringUtils.hasLength(entityName)
+                && !entityName.toLowerCase().contains("entreprise")
+                && !entityName.toLowerCase().contains("roles")
+                && StringUtils.hasLength(idEntreprise)) {
 
-        if (sql.contains("where")) {
-          sql = sql + " and " + entityName + ".identreprise = " + idEntreprise;
-        } else {
-          sql = sql + " where " + entityName + ".identreprise = " + idEntreprise;
+          if (sql.contains("where")) {
+            sql = sql + " and " + entityName + ".identreprise = " + idEntreprise;
+          } else {
+            sql = sql + " where " + entityName + ".identreprise = " + idEntreprise;
+          }
         }
+      } catch(StringIndexOutOfBoundsException e){
+        System.out.println("Erreur index: "+e.getMessage());
       }
     }
     return super.onPrepareStatement(sql);
